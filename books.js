563e10d8ee86aaf3ca8fbe6d5c0bc34fbe29192c
BORGAR8 <422004495@ntc.edu.ph>
2025-11-06 14:04:14 +0800

    Create books.js

---
+++ books.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "9780001": {
+    title: "Node.js Fundamentals",
+    author: "John Smith",
+    reviews: {}
+  },
+  "9780002": {
+    title: "Learning Express",
+    author: "Jane Doe",
+    reviews: {}
+  },
+  "9780003": {
+    title: "Mastering JavaScript",
+    author: "Alex Johnson",
+    reviews: {}
+  }
+};
